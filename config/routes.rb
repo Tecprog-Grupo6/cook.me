4c0a41d7f7773fbf8f15676ea1492d48cded338a
Mateus de Morais <mateusmorais@hotmail.com.br>
2017-04-07 19:13:13 -0300

    Add users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,21 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   as :user do
-    get '/login', to: 'devise/sessions#new'
-    post '/login', to: 'devise/sessions#create'
-    get '/logout', to: 'devise/sessions#destroy'
-    get '/perfil/senha/recuperar', to: 'devise/passwords#new'
-    get '/perfil/senha/editar', to: 'devise/passwords#edit'
-    patch '/perfil/senha/atualizar',to: 'devise/passwords#update'
-    put '/perfil/senha/atualizar', to: 'devise/passwords#update'
-    post '/perfil/senha/criar', to: 'devise/passwords#create'
-    get '/perfil/cancelar', to: 'devise/registrations#cancel'
-    get '/perfil/criar', to: 'devise/registrations#new'
-    get '/perfil/editar', to: 'devise/registrations#edit'
-    patch '/perfil/atualizar', to: 'devise/registrations#update'
-    put '/perfil/atualizar', to: 'devise/registrations#update'
-    delete '/perfil/excluir', to: 'devise/registrations#destroy'
-    post '/perfil/criar', to: 'devise/registrations#create'
+    get '/login', to: 'users/sessions#new'
+    post '/login', to: 'users/sessions#create'
+    get '/logout', to: 'users/sessions#destroy'
+    get '/perfil/senha/recuperar', to: 'users/passwords#new'
+    get '/perfil/senha/editar', to: 'users/passwords#edit'
+    patch '/perfil/senha/atualizar',to: 'users/passwords#update'
+    put '/perfil/senha/atualizar', to: 'users/passwords#update'
+    post '/perfil/senha/criar', to: 'users/passwords#create'
+    get '/perfil/cancelar', to: 'users/registrations#cancel'
+    get '/perfil/criar', to: 'users/registrations#new'
+    get '/perfil/editar', to: 'users/registrations#edit'
+    patch '/perfil/atualizar', to: 'users/registrations#update'
+    put '/perfil/atualizar', to: 'users/registrations#update'
+    delete '/perfil/excluir', to: 'users/registrations#destroy'
+    post '/perfil/criar', to: 'users/registrations#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
