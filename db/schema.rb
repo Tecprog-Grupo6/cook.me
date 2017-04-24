9a9fd1c0acae366c6b10c3ff122bbab1d6ba6b33
klementt <matheus_klementt@hotmail.com>
2017-04-24 20:07:31 -0300

    Create FollowAssociation migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422205151) do
+ActiveRecord::Schema.define(version: 20170424210050) do
+
+  create_table "follow_associations", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_follow_associations_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follow_associations_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follow_associations_on_follower_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
