8beba2db1e110d916183fde3d0c417e920c420b5
Anna Larissa <annalarissa.martins@hotmail.com>
2017-05-11 16:15:55 -0300

    Adding comments in perfil_controller

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -1,4 +1,6 @@
 class PerfilController < ApplicationController
+  # Description: Shoe the profile of the current user
+  # Parameters: none.
   def show
     @current_user = current_user
     @profile_user = User.find_by(username: params[:username])
@@ -11,7 +13,8 @@ class PerfilController < ApplicationController
 
     return result
   end
-
+  # Description: follow any profile the user wants
+  # Parameters: none.
   def follow
     @user_to_follow = User.find_by(username: params[:username])
     @current_user = User.find_by(username: params[:current_user_username])
@@ -22,7 +25,8 @@ class PerfilController < ApplicationController
 
     return result
   end
-
+  # Description: unfollow any profile the user follows
+  # Parameters: none.
   def unfollow
     @user_to_unfollow = User.find_by(username: params[:username])
     @current_user = User.find_by(username: params[:current_user_username])
