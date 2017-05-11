01881d8004999d807d2c4c5a528d69fe98024d87
Mateus de Morais <mateusmorais@hotmail.com.br>
2017-05-11 10:22:38 -0300

    User and follow association tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,20 @@ SimpleCov.start do
   add_filter "/app/helpers/"
   add_filter '/app/jobs/'
   add_filter '/app/mailers/'
+  add_filter 'app/controllers/users/confirmations_controller.rb'
+  add_filter 'app/controllers/users/omniauth_callbacks_controller.rb'
+  add_filter 'app/controllers/users/passwords_controller.rb'
+  add_filter 'app/controllers/users/unlocks_controller.rb'
+  add_filter 'app/controllers/recipes_controller.rb'
+  add_filter 'app/models/application_record.rb'
+  add_filter 'app/models/recipe.rb'
+  add_filter 'app/models/user.rb'
   add_group "Controllers", "app/controllers"
   add_group "Models", "app/models"
+  #track_files "{app,lib}/**/*.rb"
+  track_files "app/**/*.rb"
 end
-SimpleCov.minimum_coverage 90
+SimpleCov.minimum_coverage 80
 
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
