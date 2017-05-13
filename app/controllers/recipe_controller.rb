9ccf8ac63cd2fe66cfe38981074a815c56c5c4c9
Breno Mariz <mrzbeo@gmail.com>
2017-05-13 18:08:07 -0300

    adding edit recipe feature
    Signed-off-by: Breno Mariz <mrzbeo@gmail.com>
    Signed-off-by: Matheus Clemente <matheus_klementt@hotmail.com>

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,28 +3,50 @@ class RecipeController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:save]
 
   def show
-
+    #[TODO]
   end
 
   def new
+
     result = render template: "recipe/new.html.erb"
     return result
+
   end
 
-  def save
+  def save_new
+
     @recipe = Recipe.new(:title => params[:name], :text => params[:preparation], :served_people => params[:people], :prepare_time => params[:time])
-      if @recipe.save
-        redirect_to "/receita/visualizar/#{@recipe.id}"
-      else
-        redirect_to '/receita/criar'
-      end
+    return save(@recipe)
+
+  end
+
+  def save_old
+
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe.assign_attributes(:title => params[:name], :text => params[:preparation], :served_people => params[:people], :prepare_time => params[:time])
+    return save(@recipe)
+
   end
 
   def edit
 
+    result = render template: "recipe/edit.html.erb"
+    return result
+
   end
 
   def delete
+    #[TODO]
+  end
+
+  private
+  def save (to_save_recipe)
+
+    if to_save_recipe.save
+      redirect_to "/receita/visualizar/#{@recipe.id}"
+    else
+      redirect_to '/receita/criar/'
+    end
 
   end
 
