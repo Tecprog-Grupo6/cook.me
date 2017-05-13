b321641f4f31800b65248e7744642b508e4c3f04
Breno Mariz <mrzbeo@gmail.com>
2017-05-13 18:08:07 -0300

    adding delete recipe feature
    Signed-off-by: Breno Mariz <mrzbeo@gmail.com>
    Signed-off-by: Matheus Clemente <matheus_klementt@hotmail.com>

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,6 +1,6 @@
 class RecipeController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: [:save]
+  skip_before_action :verify_authenticity_token
 
   def show
     #[TODO]
@@ -8,45 +8,34 @@ class RecipeController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     result = render template: "recipe/show.html.erb"
     return result
-
   end
 
   def new
-
     result = render template: "recipe/new.html.erb"
     return result
-
   end
 
   def save_new
-
-    @recipe = Recipe.new( :title => params[:name],
-                         :text => params[:preparation],
-                         :served_people => params[:people],
-                         :prepare_time => params[:time],
-                         :user_id => params[:user_id] )
+    @recipe = current_user.recipes.create(:title => params[:name],
+                                          :text => params[:preparation],
+                                          :served_people => params[:people],
+                                          :prepare_time => params[:time])
     return save(@recipe)
-
   end
 
   def save_old
-
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe.assign_attributes( :title => params[:name],
+    @recipe.assign_attributes(:title => params[:name],
                               :text => params[:preparation],
                               :served_people => params[:people],
-                              :prepare_time => params[:time],
-                              :user_id => params[:user_id] )
+                              :prepare_time => params[:time])
     return save(@recipe)
-
   end
 
   def edit
-
-    @recipe = Recipe.find(params[:recipe_id])
-    result = render template: "recipe/edit.html.erb"
-    return result
-
+      @recipe = Recipe.find(params[:recipe_id])
+      result = render template: "recipe/edit.html.erb"
+      return result
   end
 
   def delete
@@ -55,12 +44,11 @@ class RecipeController < ApplicationController
 
   private
   def save (to_save_recipe)
-
     if to_save_recipe.save
       redirect_to "/receita/visualizar/#{@recipe.id}"
     else
       redirect_to '/receita/criar/'
     end
-
   end
+
 end
