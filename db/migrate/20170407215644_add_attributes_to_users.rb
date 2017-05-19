cdbef77625d996efbcf7e8a41c4488b94571a178
Iolane Andrade <andrade.icaa@gmail.com>
2017-05-19 11:41:44 -0300

    "Adding birthday and gender in the views (new and edit).html.erb" Signed-off-by: Matheus Clemente <matheus_klementt@hotmail.com> Signed-off-by: Iolane Andrade <andrade.icaa@gmail.com>

--- db/migrate/20170407215644_add_attributes_to_users.rb
+++ db/migrate/20170407215644_add_attributes_to_users.rb
@@ -1,6 +1,6 @@
 class AddAttributesToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :username, :string
+    add_column :users, :username, :string 
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :birthday, :date
