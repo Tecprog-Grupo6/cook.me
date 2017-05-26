2501421093010f1f6eaea32d95cdc242c9379278
Breno Mariz <mrzbeo@gmail.com>
2017-05-26 02:12:06 -0300

    Create relation between user and recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528073431) do
+
+ActiveRecord::Schema.define(version: 20170526043641) do
 
   create_table "follow_associations", force: :cascade do |t|
     t.integer  "follower_id"
