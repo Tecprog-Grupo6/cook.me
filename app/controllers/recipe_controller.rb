00fef1e4144dcb321b53d884bcc215191d15da2d
Breno Mariz <mrzbeo@gmail.com>
2017-05-31 11:19:28 -0300

    Fix recipes creation

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,6 +1,6 @@
 class RecipeController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: [:save]
+  skip_before_action :verify_authenticity_token
 
   def show
     @recipe = Recipe.find(params[:recipe_id])
@@ -14,13 +14,11 @@ class RecipeController < ApplicationController
   end
 
   def save_new
-
-    @recipe = Recipe.new(:title => params[:name], :text => params[:preparation], :served_people => params[:people], :prepare_time => params[:time])
+    @recipe = current_user.recipes.create(:title => params[:name], :text => params[:preparation], :served_people => params[:people], :prepare_time => params[:time])
     return save(@recipe)
   end
 
   def save_old
-
     @recipe = Recipe.find(params[:recipe_id])
     @recipe.assign_attributes(:title => params[:name], :text => params[:preparation], :served_people => params[:people], :prepare_time => params[:time])
     return save(@recipe)
@@ -42,7 +40,7 @@ class RecipeController < ApplicationController
       redirect_to "/receita/visualizar/#{@recipe.id}"
     else
       redirect_to '/receita/criar/'
-      end
+    end
   end
 
 end
